07fccb359439531290c7bfc6300313beeb13326e
Haruka Umino <haruka.u.71@gmail.com>
2020-05-05 17:24:55 +0900

    Updates

---
+++ precache-manifest.9921a14466c45cb2bc9b1217859e660e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5aa179eac2180ddc6851b0f4e5bba0",
+    "url": "/choiceforyou/index.html"
+  },
+  {
+    "revision": "8fee8b6995cce0cf6e59",
+    "url": "/choiceforyou/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7613c38c9a5c00e993c7",
+    "url": "/choiceforyou/static/js/2.4189a626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/choiceforyou/static/js/2.4189a626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fee8b6995cce0cf6e59",
+    "url": "/choiceforyou/static/js/main.d096e97d.chunk.js"
+  },
+  {
+    "revision": "45532d91fcc5eda5cad5",
+    "url": "/choiceforyou/static/js/runtime-main.4c03e863.js"
+  }
+]);
